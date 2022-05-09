a4c9f473891b89c6ceda1b6c5aa4f8cfca2226bb
ProjetoLC <105231827+ProjetoLC@users.noreply.github.com>
2022-05-09 13:19:56 -0300

    Add files via upload
    Primeiro Commit

---
+++ projetolc/android/app/src/main/kotlin/br/com/halana/projetolc/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.halana.projetolc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ projetolc/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'modules/historico/pages/index.dart';
+
+void main() {
+  runApp(const MyApp());
+}
